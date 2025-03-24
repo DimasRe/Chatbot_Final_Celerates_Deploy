ae623ff0b575b928948c21af5510c80f11344a0c
Dimas Arta Ramadhani <131544573+DimasRe@users.noreply.github.com>
2025-03-24 08:59:57 +0700

    Update chatbot_final_Celerates.py

--- chatbot_final_Celerates.py
+++ chatbot_final_Celerates.py
@@ -3,7 +3,7 @@ import pandas as pd
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
 
-API_KEY = "AIzaSyDh7sAqxYIUzWajFP2jC5i95uFq_CgE-_4"
+API_KEY = "AIzaSyAsIDnWtA3bUU39n95LespBednMX-63OkM"
 
 def chat(contexts, history, question):
     llm = ChatGoogleGenerativeAI(
